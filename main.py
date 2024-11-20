15cf890859edd851bc4881f78ac07b04bdc9f95f
Zhiyao(Ian) Yu <985683179yzy@gmail.com>
2024-11-19 22:48:57 -0500

    added output image on predefined intervals

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from ultralytics import YOLO
 from supervision import Detections
 from PIL import Image
 
-def detect_bounding_box(frame):
+model_path = hf_hub_download(repo_id="arnabdhar/YOLOv8-Face-Detection", cache_dir="./models", filename="model.pt")
+model = YOLO(model_path)
+
+frame_count = 0
+
+def detect_bounding_box(frame, frame_count):
     output = model(frame)
     results = Detections.from_ultralytics(output[0])
 
@@ -15,31 +20,41 @@ def detect_bounding_box(frame):
         y1 = int(face[1])
         x2 = int(face[2])
         y2 = int(face[3])
-        
+
+        if (frame_count == 50):
+            cropped_face = frame[y1:y2, x1:x2]
+            resized_image = cv2.resize(cropped_face, (160, 160))
+            filename = "output.jpg"
+            cv2.imwrite(filename, resized_image)
+
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
 
     if len(results.xyxy > 0):
         return True
 
-model_path = hf_hub_download(repo_id="arnabdhar/YOLOv8-Face-Detection", cache_dir="./models", filename="model.pt")
-model = YOLO(model_path)
 
 video_capture = cv2.VideoCapture(0)
 
 while True:
-
     result, video_frame = video_capture.read()  # read frames from the video
     if result is False:
         break  # terminate the loop if the frame is not read successfully
 
+    frame_count += 1
+
     faces = detect_bounding_box(
-        video_frame
-    )  # apply the function we created to the video frame
+        video_frame,
+        frame_count
+    )  
+
+    # Prevent overflow
+    if frame_count == 50:
+        frame_count = 0
 
     cv2.imshow(
         "Bike Gurdian", video_frame
     )
-
+    
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
